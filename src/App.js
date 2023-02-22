f6505350bb714c537546465d6f6f58777ee4c5e0
Kelly Whitlock <kellyrosewhitlock@gmail.com>
2023-02-21 23:32:39 -0500

    Fixed formatting, added Logo and Title

--- src/App.js
+++ src/App.js
@@ -10,23 +10,28 @@ import { hover } from "@testing-library/user-event/dist/hover";
 
 function App() {
   return (
-    <Grid container >
+    <Grid container 
+      direction="column"
+      //alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: '100vh' }}
+    >
       <Grid item xs={12} align="center">
         <Typography variant="h1" compact="h1" sx={{ color: "#014725" }}  style={{  fontFamily: 'SIFFON_BASIC', fontSize: "5em" }}>
           WayneHacks
         </Typography>
       </Grid>
       <Grid item xs={12} align="center" >
-        <Typography variant="h5" compact="h5" sx={{ color: "#014725",  letterSpacing: 5 }}  style={{ fontFamily: 'GlacialIndifference-Bold' }}>
+        <Typography variant="h5" compact="h5" sx={{ color: "#014725",  letterSpacing: 3 }}  style={{ fontFamily: 'GlacialIndifference-Bold' }}>
           VIRTUAL HACKATHON
         </Typography>
       </Grid>
-      <Grid item xs={12} align="center">
-        <Typography variant="h6" compact="h6" sx={{ color: "#014725", letterSpacing: 5}} style={{ fontFamily: 'GlacialIndifference-Bold', }}>
+      <Grid  item xs={12} align="center" spacing={10}>
+        <Typography variant="h6" compact="h6" sx={{ color: "#014725", letterSpacing: 3, paddingTop: 3}} style={{ fontFamily: 'GlacialIndifference-Bold', }}>
           MARCH 3-5
         </Typography>
       </Grid>
-      <Grid item xs={12} align="center">
+      <Grid item xs={12} align="center" sx={{paddingTop: 3}}>
         <Button
           sx={{
             color: '#014725',
@@ -42,11 +47,18 @@ function App() {
               color: '#014725',
             }
           }}
-          variant="contained">
+          variant="contained"
+          href="https://forms.gle/f9NVtme9KLDUFp4c8" >
           Register
         </Button>
       </Grid>
+      <Grid  item xs={12} align="center" >
+        <Typography variant="h6" compact="h6" sx={{ color: "#014725", letterSpacing: 3, paddingTop: 15}} style={{ fontFamily: 'GlacialIndifference-Bold', }}>
+          MORE INFO COMING SOON! 
+        </Typography>
+      </Grid>
     </Grid>
+    
   )
 }
 
